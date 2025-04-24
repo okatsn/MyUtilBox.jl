dcb17ed5c6d2fa39f385da15142ad534951eccad
Documenter.jl <documenter@juliadocs.github.io>
2025-04-24 03:31:24 +0000

    build based on c6f717d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyUtilBox","category":"page"},{"location":"#MyUtilBox","page":"Home","title":"MyUtilBox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyUtilBox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyUtilBox]","category":"page"}]
+}
